4f78bf24022d0c5a8e8034fac8be0aaa749e3110
Oussama Zaki <oussama.zaki@klarna.com>
2019-10-02 23:18:18 +0200

    📐 restructure to use containers

---
+++ src/app/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Configurations from '../containers/Configurations';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Configurations />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/containers/Configurations.jsx
+++ src/containers/Configurations.jsx
@@ -2,31 +2,27 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Table from './Table';
 
-import './App.css';
 import { ProductForm } from './Form';
 
-class App extends React.Component {
-  state = {
-    hide: true
-  };
+class Configurations extends React.Component {
+  state = {};
 
   toggleAdd = e => {
-    this.setState({
-      hide: !this.state.hide
-    });
+    console.log('show form here');
   };
 
   render() {
     return (
-      <div className="App">
-        <h2>Configuration Manager</h2>
-        <h4>Add and manage all your integration configs.</h4>
+      <div className="configurations-container">
+        <div className="HeaderContainer">
+          <h2>Configuration Manager</h2>
+          <h4>Add and manage all your integration configs.</h4>
+        </div>
         <Table />
         <div className="ButtonContainer">
           <Button variant="contained" color="primary" onClick={this.toggleAdd}>
             Add config
           </Button>
-          {!this.state.hide && <p>Add a new config.</p>}
         </div>
         <ProductForm />
       </div>
@@ -34,4 +30,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default Configurations;
